03497b67eda29f9a3b9edd0b61a4ccc7c01533b9
Latifah President <latifahpresident@Latifahs-MBP.lan>
2020-01-05 15:33:24 -0600

    seeded 500 users

--- database/migrations/20191231191539_users.js
+++ database/migrations/20191231191539_users.js
@@ -1,8 +1,8 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', users => {
-      users.increments().primary()
-      users.string('firebase_id').notNullable().unique()
+      users.increments()
+      users.string('firebase_id').notNullable()
       users.string('email').notNullable()
       users.string('first_name').notNullable()
       users.string('last_name').notNullable()
