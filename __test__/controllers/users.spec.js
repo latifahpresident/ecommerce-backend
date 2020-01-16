d5aacded1e346bb914cc4a6c2007c6ea68d04c5a
Latifah President <latifahpresident@Latifahs-MBP.lan>
2020-01-15 23:28:08 -0600

    worked on some error handling

--- __test__/controllers/users.spec.js
+++ __test__/controllers/users.spec.js
@@ -12,6 +12,14 @@ beforeAll(async done => {
     return done()
 });
 
+const updatedUser = {
+    firebase_id: '2435rwteray756ue57ghvders',
+    email: 'naruto9_hokage7@theleaf.com',
+    first_name: 'Naruto',
+    last_name: 'Uzumaki',
+    profile_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/itskawsar/128.jpg'
+  }
+
 describe(`Requests to /admin/ routes`, () => {
     //I will change this test once authentication is installed
     describe(`GET routes to /admin/users`, () => {
@@ -29,29 +37,33 @@ describe(`Requests to /admin/ routes`, () => {
     });
 
     it(`PUT request to /admin/user/id is successful`, async () => {
-        const updatedUser = {
-          firebase_id: '2435rwteray756ue57ghvders',
-          email: 'naruto9_hokage7@theleaf.com',
-          first_name: 'Naruto',
-          last_name: 'Uzumaki',
-          profile_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/itskawsar/128.jpg'
-        }
+      
         await request(server).put('/admin/user/2435rwteray756ue57ghvders').send(updatedUser).expect(201);
         const res = await request(server).get('/admin/users/2435rwteray756ue57ghvders');
         expect(res.status).toBe(200);
         expect(res.body.email).toBe('naruto9_hokage7@theleaf.com');
     });
 
+    it(`PUT request with no body should return 500`, async () => {
+        const res = await request(server).put('/admin/user/2435rwteray756ue57ghvders').send().expect(500);
+        expect(res.status).toBe(500);
+    });
+    
+    it(`PUT request with no id should return 400`, async () => {
+        const res = await request(server).put('/admin/user/').send(updatedUser).expect(404);
+        expect(res.status).toBe(404);
+    });
+
     it(`DELETE user is successful`, async () => {
-        const res = await request(server).delete('/admin/user/2435rwteray756ue566ttuyyy').expect(400);
+        const res = await request(server).delete('/admin/user/2435rwteray756ue566ttuyyy').expect(200);
         expect(res.body).toBe('User has been deleted');
     });
 
    
     describe(`Unsuccessful requests`, () => {
-        it(`should return 404 if id is missing`, async () => {
+        it(`should return 404 if id is missing when deleting a user`, async () => {
             const res = await request(server).delete('/admin/user/').expect(404);
-            console.log(res.body);
-        })
+            expect(res.status).toBe(404);
+        });
     })
 });
